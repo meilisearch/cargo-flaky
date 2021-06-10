07e37fb0ed1faa631a2a17532a32418a685b9a00
Irevoire <tamo@meilisearch.com>
2021-06-10 14:34:38 +0200

    fix the usage of cargo-flaky in workspaces

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ mod runner;
 use std::path::PathBuf;
 
 use cargo::core::compiler::CompileMode;
-use cargo::core::Workspace;
 use cargo::core::compiler::MessageFormat;
+use cargo::core::Workspace;
 use cargo::ops::compile;
 use cargo::ops::CompileOptions;
 use cargo::util::config::Config;
@@ -19,7 +19,6 @@ use args::Command;
 fn compile_tests(command: &Command) -> anyhow::Result<Vec<PathBuf>> {
     let manifest_path = std::env::current_dir()?.join("Cargo.toml");
     let config = Config::default().unwrap();
-    let workspace = Workspace::new(&manifest_path, &config)?;
 
     let mut options = CompileOptions::new(&config, CompileMode::Test)?;
     options.build_config.message_format = MessageFormat::Short;
@@ -29,9 +28,20 @@ fn compile_tests(command: &Command) -> anyhow::Result<Vec<PathBuf>> {
         options.build_config.requested_profile = profile;
     }
 
-    let compilation = compile(&workspace, &options)?;
+    let workspace = Workspace::new(&manifest_path, &config)?;
+    let paths = if workspace.is_virtual() {
+        let mut res = Vec::new();
+        for package in workspace.members() {
+            let workspace = Workspace::new(package.manifest_path(), &config)?;
+            let compile = compile(&workspace, &options)?;
+            let mut paths = compile.tests.into_iter().map(|c| c.path).collect();
+            res.append(&mut paths)
+        }
+        res
+    } else {
+        compile(&workspace, &options)?.tests.into_iter().map(|c| c.path).collect()
+    };
 
-    let paths = compilation.tests.into_iter().map(|c| c.path).collect();
     Ok(paths)
 }
 
@@ -40,7 +50,7 @@ fn main() -> anyhow::Result<()> {
 
     let bin_paths = compile_tests(&command)?;
 
-    let mut runner = Runner::new(bin_paths , &command.rr, command.iter, &command.test_opts);
+    let mut runner = Runner::new(bin_paths, &command.rr, command.iter, &command.test_opts);
 
     let reports = runner.run()?;
 
@@ -48,7 +58,6 @@ fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
-
 
 #[cfg(test)]
 mod test {
